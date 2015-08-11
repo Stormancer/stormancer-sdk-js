4cef5fe93d4ac29b2688aedbbb422be9d54713ff
Antoine Lafarge <ant.lafarge@gmail.com>
2015-08-11 14:53:30 +0200

    Add jsdoc tools

---
+++ doc/doc.md
@@ -0,0 +1,18 @@
+# Generate the documentation
+
+https://nodejs.org/
+https://github.com/jsdoc3/jsdoc
+http://usejsdoc.org/about-getting-started.html
+http://usejsdoc.org/about-configuring-jsdoc.html
+https://github.com/terryweiss/docstrap
+
+## Install node.js and npm
+
+install jsdoc :
+npm install jsdoc
+
+## Generate jsdoc
+
+run windows cmd and type:
+cd C:\Users\<username>\stormancer-sdk-js
+C:\Users\<username>\node_modules\.bin\jsdoc src\stormancer-sdk-js\bin\Debug -r -c doc\conf.json -d doc
--- src/stormancer-sdk-js/ApiClient.ts
+++ src/stormancer-sdk-js/ApiClient.ts
@@ -1,14 +1,36 @@
+/**
+@namespace Stormancer
+*/
 module Stormancer {
+
+    /**
+    @class ApiClient
+    */
     export class ApiClient {
+
+        /**
+        Constructor
+        @param {Object} config Configuration object
+        @param {Object} tokenHandler Token handler
+        */
         constructor(config: Configuration, tokenHandler: ITokenHandler) {
             this._config = config;
             this._tokenHandler = tokenHandler;
         }
 
         private _config: Configuration;
+
         private createTokenUri = "{0}/{1}/scenes/{2}/token";
+
         private _tokenHandler: ITokenHandler;
 
+        /**
+        Get a scene endpoint
+        @param {string} accountId Account ID
+        @param {string} applicationName Application name
+        @param {string} sceneId Scene ID
+        @param {Object} userData Some user data sent at connection
+        */
         public getSceneEndpoint<T>(accountId: string, applicationName: string, sceneId: string, userData: T): JQueryPromise<SceneEndpoint> {
             var serializer = new MsgPackSerializer();
             //var data: Uint8Array = serializer.serialize(userData);
